060e88a5943f2df6340dd30b133d022e8237083a
Maksim Tsatsura <maxim.tsatsura@gmail.com>
2026-06-10 11:23:22 +0300

    feat: CategoryFactory

---
+++ docker/db/10-create-testing-database.sql
@@ -0,0 +1,1 @@
+CREATE DATABASE vms_stone_db_test;
--- packages/box/nicole/core/src/Models/Category.php
+++ packages/box/nicole/core/src/Models/Category.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Nicole\Box\Core\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Kalnoy\Nestedset\NodeTrait;
@@ -13,37 +14,44 @@ use Spatie\Translatable\HasTranslations;
 
 class Category extends Model
 {
-    use HasExternalCode;
-    use HasSettings;
-    use HasTranslations;
-    use NodeTrait;
+  use HasExternalCode;
+  use HasSettings;
+  use HasTranslations;
+  use NodeTrait;
+  use HasFactory;
 
-    protected $fillable = [
-        'parent_id',
-        'external_code',
-        'name',
-        'slug',
-        'description',
-        'is_active',
-        'sort_order',
+  protected $fillable = [
+    'parent_id',
+    'external_code',
+    'name',
+    'slug',
+    'description',
+    'is_active',
+    'sort_order',
+  ];
+
+  public array $translatable = ['name', 'description'];
+
+  protected function casts(): array
+  {
+    return [
+      'is_active' => 'boolean',
     ];
+  }
 
-    public array $translatable = ['name', 'description'];
+  public function getFullPathAttribute(): string
+  {
+    return $this->ancestors->pluck('name')->push($this->name)->implode(' > ');
+  }
 
-    protected function casts(): array
-    {
-        return [
-            'is_active' => 'boolean',
-        ];
-    }
+  public function products(): HasMany
+  {
+    return $this->hasMany(Product::class);
+  }
 
-    public function getFullPathAttribute(): string
-    {
-        return $this->ancestors->pluck('name')->push($this->name)->implode(' > ');
-    }
+  protected static function newFactory(): \Nicole\Box\Core\Database\Factories\CategoryFactory
+  {
+    return \Nicole\Box\Core\Database\Factories\CategoryFactory::new();
+  }
 
-    public function products(): HasMany
-    {
-        return $this->hasMany(Product::class);
-    }
 }

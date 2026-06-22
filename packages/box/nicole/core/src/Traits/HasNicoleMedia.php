b63b2694c4df9339517d4371007b9ab4ea4796fb
Maksim Tsatsura <maxim.tsatsura@gmail.com>
2026-06-22 18:49:14 +0300

    update: api urls

--- packages/box/nicole/core/src/Traits/HasNicoleMedia.php
+++ packages/box/nicole/core/src/Traits/HasNicoleMedia.php
@@ -10,42 +10,38 @@ use Spatie\MediaLibrary\MediaCollections\Models\Media;
 
 trait HasNicoleMedia
 {
-  use InteractsWithMedia;
+    use InteractsWithMedia;
 
-  public function registerMediaConversions(?Media $media = null): void
-  {
-    if ($media && $media->getCustomProperty('skip_conversions')) {
-      return;
+    public function registerMediaConversions(?Media $media = null): void
+    {
+        if ($media && $media->getCustomProperty('skip_conversions')) {
+            return;
+        }
+
+        $this->addMediaConversion('preview')
+            ->fit(Fit::Max, 400, 400)
+            ->format('webp')
+            ->quality(80)
+            ->sharpen(10)
+            ->nonQueued()
+            ->performOnCollections('main');
     }
 
-    $this->addMediaConversion('preview')
-      ->fit(Fit::Max, 400, 400)
-      ->format('webp')
-      ->quality(80)
-      ->sharpen(10)
-      ->nonQueued()
-      ->performOnCollections('main');
-  }
+    public function getPreviewUrl(): ?string
+    {
+        if ($this->hasMedia('preview')) {
+            return $this->getFirstMediaUrl('preview');
+        }
 
-  public function getPreviewUrl(): ?string
-  {
-    $url = null;
+        if ($this->hasMedia('main')) {
+            return $this->getFirstMediaUrl('main', 'preview') ?:
+              $this->getFirstMediaUrl('main');
+        }
 
-    if ($this->hasMedia('preview')) {
-      $url = $this->getFirstMediaUrl('preview');
-    } elseif ($this->hasMedia('main')) {
-      $url = $this->getFirstMediaUrl('main', 'preview') ?:
-        $this->getFirstMediaUrl('main');
-    } elseif (method_exists($this, 'product') && $this->product) {
-      $url = $this->product->getPreviewUrl();
+        if (method_exists($this, 'product') && $this->product) {
+            return $this->product->getPreviewUrl();
+        }
+
+        return null;
     }
-
-    // Если ссылка относительная, преобразуем ее в полный URL
-    if ($url && !str_starts_with($url, 'http')) {
-      return url($url);
-    }
-
-    return $url ?: null;
-  }
-
 }

c438d40da4edc23151bae714ad25eec6cca63f36
Maksim Tsatsura <maxim.tsatsura@gmail.com>
2026-07-09 15:54:58 +0300

    rename branch

--- resources/js/layouts/MainLayout.tsx
+++ resources/js/layouts/MainLayout.tsx
@@ -9,6 +9,7 @@ interface MainLayoutProps extends PropsWithChildren {
   headerOverlaps?: boolean;
 }
 
+
 export default function MainLayout({ children, headerOverlaps = false }: MainLayoutProps) {
   return (
     <div className="flex flex-col min-h-screen bg-slate-50 font-sans">

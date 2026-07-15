59c631bb02e920dee7a69340e1ff449c7b8ec006
Maksim Tsatsura <maxim.tsatsura@gmail.com>
2026-07-15 10:41:46 +0300

    feat: deploy-hook.sh

--- deploy-hook.sh
+++ deploy-hook.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
+set -e # Останавливать выполнение при любых ошибках
 
-echo "Загрузка и обновление виджета cpq-stone..."
+BRANCH_NAME=${1:-"deploy/build"}
+
+echo "Обновление статических файлов виджета калькулятора (Ветка: $BRANCH_NAME)..."
+
 rm -rf public/cpq-stone
-git clone -b deploy/build --single-branch git@github.com:kapitulin24/cpq-stone-calc.git public/cpq-stone
-rm -rf public/cpq-stone/.git
-echo "Виджет cpq-stone успешно добавлен."
+
+git clone -b "$BRANCH_NAME" --single-branch git@github.com:kapitulin24/cpq-stone-calc.gitpublic/cpq-stone
+
+rm -rf public/widget/.git
+
+chown -R www-data:www-data public/cpq-stone
+chmod -R 775 public/cpq-stone
+
+echo "Виджет успешно обновлен."

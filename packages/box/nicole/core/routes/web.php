bcd49c42400a4cf30b14ba382fa1f51165710a32
Maksim Tsatsura <maxim.tsatsura@gmail.com>
2026-07-15 10:16:15 +0300

    chore: add nicole core as git submodule

--- packages/box/nicole/core/routes/web.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-declare(strict_types=1);
-
-use Illuminate\Support\Facades\Route;
-use Nicole\Box\Core\Http\Controllers\CalculatorController;
-
-Route::get('/calculator/{type?}', [CalculatorController::class, 'show'])
-  ->name('calculator.show');
